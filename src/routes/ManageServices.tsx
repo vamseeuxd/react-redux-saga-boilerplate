a65d460fb994bbf222b10bd74d8fc930104979ad
DESKTOP-15JEVLE\Vamsi kalyan <vamsi.flex@gmail.com>
2021-01-23 18:47:46 +0530

    IDE Webstorm

--- src/routes/ManageServices.tsx
+++ src/routes/ManageServices.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
 function ManageServices() {
+  // @ts-ignore
   return (
     <div className="app-page">
-      <h4 className="mt-4 pb-3 border-bottom">Manage Services</h4>
       <div>
-        <img width="100%" src="/media/screens/calendar.jpg" />
+        <h4 className="mt-4 pb-3 border-bottom">Manage Services</h4>
       </div>
     </div>
   );
 }
 
 export default ManageServices;
+
+/* https://fullcalendar.io/docs/react*/
